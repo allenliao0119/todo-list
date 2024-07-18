0de4133b2219cb143c963695df24c2629fb93451
AllenLiao <allen10153@gmail.com>
2024-07-18 12:34:49 +0800

    router init

--- app.js
+++ app.js
@@ -9,6 +9,38 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// create todo
+app.get('/todos/new', (req, res) => {
+  res.send('create todo page.')
+})
+
+app.post('/todos', (req, res) => {
+  res.send('todo has been created.')
+})
+
+// read todo
+app.get('/todos', (req, res) => {
+  res.send('read all todos.')
+})
+
+app.get('/todos/:id', (req, res) => {
+  res.send(`read todo: ${req.params.id}.`)
+})
+
+// update todo
+app.get('/todos/:id/edit', (req, res) => {
+  res.send(`todo: ${req.params.id} edit page.`)
+})
+
+app.put('/todos/:id', (req, res) => {
+  res.send(`todo: ${req.params.id} has been modified.`)
+})
+
+// delete todo
+app.delete('/todos/:id', (req, res) => {
+  res.send(`todo: ${req.params.id} has been deleted.`)
+})
+
 // ----start to listen on port----
 app.listen(port, (req, res) => {
   console.log(`App is running on http://localhost:${port}`)
