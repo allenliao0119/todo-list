61f7285266841343a3b6a7d3f4597f35f78acbc1
AllenLiao <allen10153@gmail.com>
2024-07-18 13:02:47 +0800

    add todo seeder

---
+++ seeders/20240718045809-initial-data.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await (queryInterface.bulkInsert('Todos', 
+      Array.from({length:10}).map((_, i) => {
+        return {
+          name: `todo-${i}`
+        }
+      })
+    ))
+  },
+
+  async down (queryInterface, Sequelize) {
+    await (queryInterface.bulkDelete('Todos', null))
+  }
+};
