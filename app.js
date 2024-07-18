574dec594d41bac889c88ddfe577a6b37edbe13e
AllenLiao <allen10153@gmail.com>
2024-07-18 14:20:48 +0800

    implement updating todo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const {engine} = require('express-handlebars')
+const methodOverride =require('method-override')
 
 const port = 3000
 
@@ -14,6 +15,7 @@ app.set('views', './views')
 
 // ----define routes----
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 // initialize
 app.get('/', (req, res) => {
@@ -52,11 +54,19 @@ app.get('/todos/:id', (req, res) => {
   
 // update todo
 app.get('/todos/:id/edit', (req, res) => {
-  res.send(`todo: ${req.params.id} edit page.`)
+  const id = req.params.id
+  return Todo.findByPk(id, {
+    attributes: ['id', 'name'],
+    raw: true
+  })
+    .then(todo => res.render('edit', {todo}))
 })
 
 app.put('/todos/:id', (req, res) => {
-  res.send(`todo: ${req.params.id} has been modified.`)
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.update({name}, {where: {id}})
+    .then(() => res.redirect(`/todos/${id}`))
 })
 
 // delete todo
