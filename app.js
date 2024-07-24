cae127c4bad746cc3ac0228927085b61ad7a1d4d
AllenLiao <allen10153@gmail.com>
2024-07-24 13:27:32 +0800

    handle flash message by using message/error middleware

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const flash = require('connect-flash')
 const session = require('express-session')
+const app = express()
+
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
 
-const port = 3000
 const router = require('./routes')
+const messageHandler = require('./middlewares/message-handler')
+const errorHandler = require('./middlewares/error-handler')
 
-const app = express()
+const port = 3000
 
 app.engine('hbs', engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
@@ -23,7 +26,11 @@ app.use(session({
 }))
 app.use(flash())
 
+app.use(messageHandler)
+
 app.use(router)
+
+app.use(errorHandler)
 
 // ----start to listen on port----
 app.listen(port, (req, res) => {
