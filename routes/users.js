2273a0a906e7e5229dcdbcf909886bc6ab7cb56b
AllenLiao <allen10153@gmail.com>
2024-07-30 14:31:03 +0800

    implement register

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,46 @@
 const express = require('express')
 const router = express.Router()
 
+const db = require('../models')
+const User = db.User
+
 // register
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
 router.post('/', (req, res) => {
-  res.send('create user.')
+  const user = req.body
+  if (user.email === '' || user.password === '') {
+    req.flash('error', 'email 及密碼為必填') 
+    return res.redirect('back')
+  }
+
+  if (user.password !== user.passwordConfirm) {
+    req.flash('error', '驗證密碼與密碼不符')
+    return res.redirect('back')
+  } 
+  
+  return User.findOrCreate({
+    where: {email: user.email},
+    defaults: {
+      password: user.password
+    }
+  })
+    .then(result => {
+      const [user, existed] = result
+      if (existed) {
+        req.flash('success', '註冊成功')
+        res.redirect('/users/login')
+      } else {
+        req.flash('error', 'email 已註冊')
+        res.redirect('back')
+      }
+    })
+    .catch(error => {
+      error.error_msg = '註冊失敗'
+      next(error)
+    })
 })
 
 // login
@@ -16,6 +49,7 @@ router.get('/login', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+
   res.send('user login')
 })
 
